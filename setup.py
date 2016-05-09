82441eedc8ae14f34ce597b219b1c09a7389b73b
Will McGugan <willmcgugan@gmail.com>
2016-05-09 16:25:30 +0100

    M2M client extracted from dpcore

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+]
+
+# http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+with open('m2mclient/_version.py') as f:
+    exec(f.read())
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(
+    name='m2mclient',
+    version=VERSION,
+    description="A client for the Dataplicity M2M server",
+    long_description=long_desc,
+    author='WildFoundry',
+    author_email='support@dataplicity.com',
+    url='https://www.dataplicity.com',
+    platforms=['any'],
+    packages=find_packages(),
+    classifiers=classifiers,
+
+    install_requires=[
+        'websocket-client',
+        'fs>=0.5.0',
+        'enum34',
+        'six',
+        'python-daemon==2.1.1',
+    ]
+)
