e67acccc46a8148a39619d4eb82a570376021732
Will McGugan <willmcgugan@gmail.com>
2017-01-10 10:03:00 +0000

    This was a default local test key only, but specifying one means users are not forced to create their own key and by default would use this key.  Remove this default to force users to specify their own local test keys.

--- m2mclient/__main__.py
+++ m2mclient/__main__.py
@@ -12,7 +12,7 @@ log = logging.getLogger('m2m')
 m2m_client = M2MClient(
     'ws://127.0.0.1:8080/m2m/',
     'dataplicity',
-    '247e753e-7003-11e4-b2aa-474b3c38e550'
+    'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
 )
 
 
